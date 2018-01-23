b4dc30b18606177afbb44436874960bd9de1b760
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2018-01-24 01:50:16 +0530

    feat: add a div and instantiate ace editor with HTML mode on it on load

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import Elm from './src/Main.elm';
+import './src/Main.css';
 
-Elm.Main.embed(document.querySelector('#app'));
+const app = Elm.Main.embed(document.querySelector('#app'));
+
+app.ports.init.subscribe(() => {
+  const html = ace.edit('html');
+  html.session.setMode('ace/mode/html');
+  const elm = ace.edit('elm');
+  elm.session.setMode('ace/mode/elm');
+});
--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,53 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (id)
+import Ports
 
 
-main : Html.Html msg
+main : Program Never Model Msg
 main =
-    text "Hello, world!"
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = always Sub.none
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    ()
+
+
+init : ( Model, Cmd Msg )
+init =
+    () ! [ Ports.init () ]
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Msg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    model ! []
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [ id "main" ]
+        [ div [ id "html" ] []
+        , div [ id "elm" ] []
+        ]
