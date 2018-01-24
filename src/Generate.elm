d7ddebcff7cb030ce3c69cfa8763fbce92cfbc07
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2018-01-24 20:10:37 +0530

    feat: add first real implementation of Generate.generate
    From some manual testing, the output looks identical to elm-format except that
    elm-format adds a blank line above all comments, which I'm intentionally not
    adding for now.

--- src/Generate.elm
+++ src/Generate.elm
@@ -5,4 +5,67 @@ import Node exposing (..)
 
 generate : Node -> String
 generate node =
-    toString node
+    case node of
+        Comment value ->
+            value
+                |> String.split "\n"
+                |> List.map (\line -> "-- " ++ String.trim line)
+                |> String.join "\n"
+
+        Element name attributes children ->
+            let
+                a =
+                    "node " ++ toString name
+
+                b =
+                    attributes
+                        |> List.map
+                            (\( name, value ) ->
+                                "attribute " ++ toString name ++ " " ++ toString value
+                            )
+                        |> String.join ", "
+
+                c =
+                    children
+                        |> List.indexedMap
+                            (\i node ->
+                                let
+                                    prefix =
+                                        case ( i, node ) of
+                                            ( 0, _ ) ->
+                                                ""
+
+                                            ( _, Comment _ ) ->
+                                                ""
+
+                                            _ ->
+                                                ", "
+                                in
+                                prefix ++ generate node
+                            )
+                        |> String.join "\n"
+
+                nested string newline =
+                    indent <|
+                        case ( string, newline ) of
+                            ( "", _ ) ->
+                                "[]"
+
+                            ( _, False ) ->
+                                "[ " ++ string ++ " ]"
+
+                            ( _, True ) ->
+                                "[ " ++ string ++ "\n]"
+            in
+            [ a, nested b False, nested c True ] |> String.join "\n"
+
+        Text value ->
+            "text " ++ toString value
+
+
+indent : String -> String
+indent string =
+    string
+        |> String.split "\n"
+        |> List.map (\line -> "    " ++ line)
+        |> String.join "\n"
